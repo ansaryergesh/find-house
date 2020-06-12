9582df59e046b9d14665b0782a93cc4ce4f8ba70
ansaryergesh <yergeshovansar@gmail.com>
2020-06-12 18:45:39 +0600

    user model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_064156) do
+ActiveRecord::Schema.define(version: 2020_06_12_124241) do
 
   create_table "houses", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
     t.decimal "rating"
-    t.datetime "created_at", precision: 6, null: false 
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
