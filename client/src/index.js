5573ab1d92e9821d96feeba7b2b98f96458c7035
ansaryergesh <yergeshovansar@gmail.com>
2020-06-14 12:22:55 +0600

    new folders for react

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
